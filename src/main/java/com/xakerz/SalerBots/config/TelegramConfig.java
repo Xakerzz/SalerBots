66655d745ae679e9a33b39ef78aa725623c9ea2a
xakerzz <xakerz@me.com>
2024-07-08 00:27:54 +0300

    new commite

--- src/main/java/com/xakerz/SalerBots/config/TelegramConfig.java
+++ src/main/java/com/xakerz/SalerBots/config/TelegramConfig.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class TelegramConfig {
 
-    //https://api.telegram.org/bot5994666145:AAF606nhEp2R5_EBcKgDPQqq2f_Vu_JgtMQ/setWebhook?url=https://c3bf-2a04-5200-fff5-00-7f6.ngrok-free.app
+    //https://api.telegram.org/bot5994666145:AAF606nhEp2R5_EBcKgDPQqq2f_Vu_JgtMQ/setWebhook?url=https://a5vto4-46-17-104-29.ru.tuna.am
     @Value("${telegrambot.name}")
     private  String botName;
     @Value("${telegrambot.token}")
